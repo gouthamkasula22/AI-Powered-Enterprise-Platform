d2b15769e670e391923add5ce441f85fcb9f0587
Goutham Kasula <gouthamkasula22@gmail.com>
2025-10-15 11:23:52 -0400

    Cleanup: Remove unused files, organize docs

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import AppTest from './AppTest.jsx'  // Import test app
 import './index.css'
-
-// Use TEST_MODE to toggle between production and test app
-const TEST_MODE = false;
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {TEST_MODE ? <AppTest /> : <App />}
+    <App />
   </React.StrictMode>,
 )
